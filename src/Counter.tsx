a415e5387d745582b91a1f9a1cda968a9aa537a0
gjuanillo <gjuanillo.dev@gmail.com>
2025-06-28 15:17:54 +0800

    Use State With Strings

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 
 const Counter: React.FC = () => {
-    const [count, setCount] = useState<number>(0);
+    const message : string = "Text";
+    const [count, setCount] = useState<string>(message);
+
+    const message1 : string = "UpdatedText1";
+    const message2 : string = "UpdatedText2";
 
     const increment = () => {
-        setCount(count + 1);
+        setCount(message1);
     };
     const decrement = () => {
-        setCount(count - 1);
+        setCount(message2);
     };
 
     return (
         <div className='contact-section'>
             <p>Count: {count}</p>
-            <button className='counterButton' onClick={increment}>Increase</button>
-            <button className='counterButton' onClick={decrement}>Decrease</button>
+            <button className='counterButton' onClick={increment}>Update</button>
+            <button className='counterButton' onClick={decrement}>Update</button>
         </div>
     );
 };
