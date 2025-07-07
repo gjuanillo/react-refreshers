0ddd0ea4a1e708583f20cd12daf35d8802febe11
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-07 19:44:21 +0800

    useState with Arrays/Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css'
-import Counter from './Counter'
+import { useState } from 'react';
 
 function App() {
+
+    const [counters, setCounters] = useState([{ id: 1, value: 0 }]);
+
+    const addCounter = () => {
+        setCounters([...counters, { id: (counters.length + 1), value: 0 }]);
+    };
+
+    const incrementCounter = (id: number) => {
+        setCounters(
+            counters.map(c => c.id === id ? { ...c, value: (c.value + 1) } : c)
+        );
+    }
+
     return (
-    <Counter/>
-    )
+        <>
+            <button onClick={addCounter}>Add Counter</button>
+            <ul>
+                {counters.map(c => (
+                    <li key={c.id}>
+                        Counter {c.id}: {c.value}
+                        <button onClick={() => incrementCounter(c.id)}>Increment</button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
-export default App
+export default App;
