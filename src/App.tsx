3b49d88fa341da47dcf304d205d4776ec6a2fab9
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-08 08:07:15 +0800

    Cleanup Main App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
 import './App.css'
-import { useState } from 'react';
-import Counter from './Counter';
 
-export interface CounterType {
-    id: number;
-    value: number;
-}
 
 function App() {
-    const [counters, setCounters] = useState<CounterType[]>([{ id: 1, value: 0 }]);
-
-    const addCounter = () => {
-        const newId = counters.length > 0 ? counters[counters.length - 1].id + 1 : 1;
-        setCounters([...counters, { id: newId, value: 0 }]);
-    };
-
-    const incrementCounter = (id: number) => {
-        setCounters(prev =>
-            prev.map(c => (c.id === id ? { ...c, value: c.value + 1 } : c))
-        );
-    };
-
-    const decrementCounter = (id: number) => {
-        setCounters(prev =>
-            prev.map(c => (c.id === id ? { ...c, value: c.value - 1 } : c))
-        );
-    };
-
     return (
         <>
-            <button onClick={addCounter}>Add Counter</button>
-            <ul>
-                {counters.map(counter => (
-                    <Counter
-                        key={counter.id}
-                        counter={counter}
-                        onIncrement={() => incrementCounter(counter.id)}
-                        onDecrement={() => decrementCounter(counter.id)}
-                    />
-                ))}
-            </ul>
+            <h1>Text</h1>
         </>
     );
 }
