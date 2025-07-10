b9e2f83ab1ca2b785b88099d4fb5b31ae3be5233
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-10 16:22:09 +0800

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css'
-import DigitalClock from './DigitalClock';
 
 function App() {
 
+    const [stateCount, setStateCount] = useState(0);
+    const refCount = useRef(0);
+
+    useEffect(() => {
+        console.log('Component Re-rendered');
+    });
+
+    const incrementStateCount = () => {
+        setStateCount(stateCount + 1);
+    }
+
+    const incrementRefCount = () => {
+        refCount.current += 1;
+        console.log(`Ref Count ${refCount.current}`)
+    }
+
     return (
         <>
-            <DigitalClock/>
+            <div>
+                <p>State Count:  {stateCount}</p>
+                <button onClick={incrementStateCount}>Increment</button>
+
+                <p>Ref Count:  {refCount.current}</p>
+                <button onClick={incrementRefCount}>Increment</button>
+            </div>
         </>
     )
 
