acbbc97502134481968fad63fc19f6c0d4ac1395
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-11 14:36:21 +0800

    Custom Hooks with Default Argument

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css'
 import useCounter from './useCounter';
 
 function App() {
-    const {count, increment, decrement} = useCounter();
+    const {count, increment, decrement, reset} = useCounter(0);
     return (
         <>
             <h2>Count: {count}</h2>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
         </>
     )
 }
--- src/useCounter.tsx
+++ src/useCounter.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-function useCounter() {
+function useCounter(initialValue : number = 0) {
     const [count, setCount] = useState<number>(0);
-    const increment = () => setCount(count+1);
-    const decrement = () => setCount(count-1);
-    return {count, increment, decrement}
+    const increment = () : void => setCount(count+1);
+    const decrement = () : void => setCount(count-1);
+    const reset = () => setCount(initialValue);
+    return {count, increment, decrement, reset}
 }
 
 export default useCounter;
