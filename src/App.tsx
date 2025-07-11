042d5b9da668611045f62fb3d30cb95f885ba5b3
gjuanillo <gjuanillo.dev@gmail.com>
2025-07-11 14:29:09 +0800

    Custom Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
-import { ThemeContext } from './ThemeContext';
 import './App.css'
-import ComponentA from './ComponentA';
-import { useState } from 'react';
+import useCounter from './useCounter';
 
 function App() {
-    type Theme = 'light' | 'dark';
-    const [theme, setTheme] = useState<Theme>('dark');
-
-    const toggleTheme = (): void => {
-        setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
-    }
-
-    const changeBackground = (theme: Theme): string => {
-        return theme === 'light' ? '#e3d1cd' : '#242424'
-    }
-
-    const containerStyle = {
-        border: '2px solid black',
-        padding: '10px',
-        justifyItems: 'center',
-        backgroundColor: changeBackground(theme)
-    }
-
+    const {count, increment, decrement} = useCounter();
     return (
         <>
-        <ThemeContext.Provider value={theme}>
-            <div style={containerStyle}>
-                <h2>App Component</h2>
-                <button onClick={toggleTheme}>Toggle Theme</button>
-                <ComponentA />
-            </div>
-        </ThemeContext.Provider>
+            <h2>Count: {count}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </>
     )
-
 }
 
 export default App;
